4ee1851bf47d7edfa8a0673ab29f21dbbd09307c
Toshiki Ito <toshiki1098@gmail.com>
2023-12-19 18:45:17 +0900

    テストの実装

---
+++ src/__tests__/main.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Home from '../app/page'
+import {Button, Link} from '@/components/Elements'
+
+describe('Home', () => {
+  test('ヘッダー検証', () => {
+    render(<Button/>)
+
+    const button = screen.getByRole('button')
+
+    expect(button).toBeInTheDocument()
+  })
+})
